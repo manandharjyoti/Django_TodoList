a486e839e6e856720bfd176d965d2d5301b3cf56
Jyoti Manandhar <manandharjyoti@gmail.com>
2020-12-12 20:53:20 +0545

    change in view

--- todo/views.py
+++ todo/views.py
@@ -12,14 +12,14 @@ def todolist_view(request):
     today = datetime.now().date()
     todolist = Task.objects.filter(user=request.user)
     # user = User.objects.all()
-    context = {"todolist": todolist, "today":today}
+    context = {"todolist": todolist, "today": today}
     return render(request, "todolist.html", context)
-    
+
 
 def addlist_view(request):
     form = TaskForm(request.POST or None)
     if form.is_valid():
-        Task = form.save(commit = False)
+        Task = form.save(commit=False)
         Task.user = request.user
         Task = Task.save()
         # form.save()
@@ -29,28 +29,17 @@ def addlist_view(request):
         form = TaskForm()
     return render(request, "form.html", {"form": form})
 
-def delete_view(request,task_id):
+
+def delete_view(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     task.delete()
     return redirect(reverse("todo:todolist_view"))
 
+
 def edit_view(request, task_id):
     task = get_object_or_404(Task, id=task_id)
-    form = TaskForm(request.POST or None, instance= task)
+    form = TaskForm(request.POST or None, instance=task)
     if form.is_valid():
         form.save()
         return HttpResponseRedirect(reverse("todo:todolist_view"))
-    return render(request,"form.html",{"form":form})
-
-    # def edit_author(request,author_id):
-    #     author = get_object_or_404(Author, id=author_id)
-    #     form = AuthorForm(request.POST or None , instance=author)
-    #     if form.is_valid():
-    #         form.save()
-    #         return HttpResponseRedirect(reverse("apps:author_list"))
-    #     return render(request, "apps/author_form.html", {"form":form})
-
-    
-
-
-
+    return render(request, "form.html", {"form": form})
